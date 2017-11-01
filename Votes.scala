83eb0fb7406d794d5c18d4eb9e3eab08210ed984
Juan Manuel <rodriguezjuanmanuelar@gmail.com>
2017-10-31 21:03:16 -0300

    reduce votes

--- Votes.scala
+++ Votes.scala
@@ -1,11 +1,29 @@
 import scala.io.Source
+import collection.mutable.HashMap
 
 object Test{
   def main(args:Array[String]){
     println("Hola votante")
     val file = Source.fromFile("./csv/AcumuladoVotos.csv")
-    val fileAux = file.getLines.foreach(_.split(';').foreach(println))
-    
+    println("///////////////////////////////////////////////////////////////////////////////////////////////////////")
+
+
+    val contarVotos = (x: Int, y: Int) => {
+      val winner = x + y
+      println(s"compared $x to $y, $winner was larger")
+      winner
+    }
+
+
+    var x = new HashMap[String,String](){}
+    //val fileAux2 = file.getLines().toList.foreach(y => x += (y.split(";")(0) -> y.split(";")(5)))
+    val fileAux2 = file.getLines().toList
+    fileAux2.foreach(y => x += (y.split(";")(0) -> fileAux2.reduceLeft(_ + _.split(";")(5))))
+
     file.close()
+
+
+    x.foreach(println)
+    println(x("Buenos Aires"))
   }
 }
